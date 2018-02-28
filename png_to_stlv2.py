cc3eebf09854030db208d68c55a205627dd23dd7
aether <aether@ubuntu.attlocal.net>
2018-02-27 16:13:56 -0800

    pngs read in order

--- png_to_stlv2.py
+++ png_to_stlv2.py
@@ -8,8 +8,24 @@ import os
 import numpy
 import mcubes
 from pymesh import stl, obj
+import re
 
 # python2 png_to_stlv2.py -p /Users/aether/Documents/Dicom2Mesh/testing -o testing
+
+# Test if a given chunk is an int
+def tryint(c):
+	try:
+		return int(c)
+	except:
+		return c
+
+# turn a string into a list of string and number chunks
+def alphanum_key(s):
+	return [tryint(c) for c in re.split('([0-9]+)', s)]
+
+# sort the given list
+def natural_sort(list):
+	list.sort(key=alphanum_key)
 
 if __name__ == '__main__':
 	# construct the argument parse and parse the arguments
@@ -25,12 +41,14 @@ if __name__ == '__main__':
 	print(args["path"])
 
 	for dirName, subdirList, fileList in os.walk(args["path"]):
+		natural_sort(fileList)
 		for filename in fileList:
 			print(filename)
 			if ".png" in filename.lower():  # check whether the file's png
 				img = misc.imread(os.path.join(dirName,filename), flatten=True)
 				print(img.shape)
 				all_imgs.append(img)
+
 
 	all_imgs = np.dstack(all_imgs)
 
@@ -49,10 +67,14 @@ if __name__ == '__main__':
 	print "Done with marching cubes algorithm"
 
 	# Export the result
-	mcubes.export_obj(vertices, triangles, args["output"] + ".obj")
+	mcubes.export_obj(vertices, triangles, "3D_Models/" + args["output"] + ".obj")
 	print "File saved as obj"
 
 	# Convert to stl
-	m = obj.Obj(args["output"] + ".obj")
-	m.save_stl(args["output"] + ".stl")
+	m = obj.Obj("3D_Models/" + args["output"] + ".obj")
+	m.save_stl("3D_Models/" + args["output"] + ".stl")
+
+
+
+
 	print "Converted to stl"

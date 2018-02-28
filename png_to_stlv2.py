251575510133d53cc07e24dfa915274401d0f34e
victorvong <victorthevong@gmail.com>
2018-02-27 18:38:28 -0800

    better marching cubes

--- png_to_stlv2.py
+++ png_to_stlv2.py
@@ -71,76 +71,87 @@ if __name__ == '__main__':
 	# verts, faces, normals, values = measure.marching_cubes_lewiner(ArrayDicom, 0.0) 
 	verts, faces, normals, values = measure.marching_cubes_lewiner(ArrayDicom, 0.0, allow_degenerate=False) # takes away triangles with 0 area, slows down algo
 
-	import numpy as np
-	import dicom
-	import os
-	import matplotlib.pyplot as plt
-	from glob import glob
-	from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-	import scipy.ndimage
-	from skimage import morphology
-	from skimage import measure
-	from skimage.transform import resize
-	from sklearn.cluster import KMeans
-	from plotly import __version__
-	from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-	from plotly.tools import FigureFactory as FF
-	from plotly.graph_objs import *
+	# Export the result
+	mcubes.export_obj(verts, faces, "victor.obj")
+	print "File saved as obj"
+	# Convert to stl
+	m = obj.Obj("victor.obj")
+	m.save_stl("victor.stl")
+	print "Converted to stl"
 
-	def plotly_3d(verts, faces):
-		x,y,z = zip(*verts) 
+
+################################################################
+	
+	# import numpy as np
+	# import dicom
+	# import os
+	# import matplotlib.pyplot as plt
+	# from glob import glob
+	# from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+	# import scipy.ndimage
+	# from skimage import morphology
+	# from skimage import measure
+	# from skimage.transform import resize
+	# from sklearn.cluster import KMeans
+	# from plotly import __version__
+	# from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+	# from plotly.tools import FigureFactory as FF
+	# from plotly.graph_objs import *
+
+	# def plotly_3d(verts, faces):
+	# 	x,y,z = zip(*verts) 
 		
-		print "Drawing"
+	# 	print "Drawing"
 		
-		# Make the colormap single color since the axes are positional not intensity. 
-	#    colormap=['rgb(255,105,180)','rgb(255,255,51)','rgb(0,191,255)']
-		colormap=['rgb(236, 236, 212)','rgb(236, 236, 212)']
+	# 	# Make the colormap single color since the axes are positional not intensity. 
+	# #    colormap=['rgb(255,105,180)','rgb(255,255,51)','rgb(0,191,255)']
+	# 	colormap=['rgb(236, 236, 212)','rgb(236, 236, 212)']
 		
-		fig = FF.create_trisurf(x=x,
-							y=y, 
-							z=z, 
-							plot_edges=False,
-							colormap=colormap,
-							simplices=faces,
-							backgroundcolor='rgb(64, 64, 64)',
-							title="Interactive Visualization")
-		iplot(fig)
+	# 	fig = FF.create_trisurf(x=x,
+	# 						y=y, 
+	# 						z=z, 
+	# 						plot_edges=False,
+	# 						colormap=colormap,
+	# 						simplices=faces,
+	# 						backgroundcolor='rgb(64, 64, 64)',
+	# 						title="Interactive Visualization")
+	# 	iplot(fig)
 
-	def plt_3d(verts, faces):
-		print "Drawing"
-		x,y,z = zip(*verts) 
-		fig = plt.figure(figsize=(10, 10))
-		ax = fig.add_subplot(111, projection='3d')
+	# def plt_3d(verts, faces):
+	# 	print "Drawing"
+	# 	x,y,z = zip(*verts) 
+	# 	fig = plt.figure(figsize=(10, 10))
+	# 	ax = fig.add_subplot(111, projection='3d')
 
-		# Fancy indexing: `verts[faces]` to generate a collection of triangles
-		mesh = Poly3DCollection(verts[faces], linewidths=0.05, alpha=1)
-		face_color = [1, 1, 0.9]
-		mesh.set_facecolor(face_color)
-		ax.add_collection3d(mesh)
+	# 	# Fancy indexing: `verts[faces]` to generate a collection of triangles
+	# 	mesh = Poly3DCollection(verts[faces], linewidths=0.05, alpha=1)
+	# 	face_color = [1, 1, 0.9]
+	# 	mesh.set_facecolor(face_color)
+	# 	ax.add_collection3d(mesh)
 
-		ax.set_xlim(0, max(x))
-		ax.set_ylim(0, max(y))
-		ax.set_zlim(0, max(z))
-		ax.set_axis_bgcolor((0.7, 0.7, 0.7))
-		plt.show()
+	# 	ax.set_xlim(0, max(x))
+	# 	ax.set_ylim(0, max(y))
+	# 	ax.set_zlim(0, max(z))
+	# 	ax.set_axis_bgcolor((0.7, 0.7, 0.7))
+	# 	plt.show()
 
 
-	plt_3d(verts, faces)
+	# # plt_3d(verts, faces)
 
 
 ################################################################
 
-	# import vtkInterface
-	# # pip2 install vtk
-	# # pip2 install vtkInterface
-	# # Filetype must be either "ply", "stl", "g3d" or "vtk"
-	# # http://vtkinterface.readthedocs.io/en/latest/polydata.html#mesh-manipulation-and-plotting
-	# # mesh = vtkInterface.PolyData(args["output"] + ".stl")
-	# mesh = vtkInterface.PolyData("pelvis_thresh_full.stl")
-	# mesh.Clean()
-	# mesh = mesh.TriFilter()
-	# # mesh = mesh.Subdivide(1, subfilter="loop")
-	# mesh.Plot(color='orange')
+	import vtkInterface
+	# pip2 install vtk
+	# pip2 install vtkInterface
+	# Filetype must be either "ply", "stl", "g3d" or "vtk"
+	# http://vtkinterface.readthedocs.io/en/latest/polydata.html#mesh-manipulation-and-plotting
+	# mesh = vtkInterface.PolyData(args["output"] + ".stl")
+	mesh = vtkInterface.PolyData("victor.stl")
+	mesh.Clean()
+	mesh = mesh.TriFilter()
+	# mesh = mesh.Subdivide(1, subfilter="loop")
+	mesh.Plot(color='orange')
 
 ################################################################
 

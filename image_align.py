171caecc17478f920dcb93776fba506442a8c252
aether <aether@ubuntu.attlocal.net>
2018-03-02 14:35:06 -0800

    align images

--- image_align.py
+++ image_align.py
@@ -8,51 +8,65 @@ GOOD_MATCH_PERCENT = 0.15
 
 def alignImages(im1, im2):
 
-  # Convert images to grayscale
-  im1Gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
-  im2Gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-  
-  # Detect ORB features and compute descriptors.
-  orb = cv2.ORB_create(MAX_MATCHES)
-  keypoints1, descriptors1 = orb.detectAndCompute(im1Gray, None)
-  keypoints2, descriptors2 = orb.detectAndCompute(im2Gray, None)
-  
-  # Match features.
-  matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
-  matches = matcher.match(descriptors1, descriptors2, None)
-  
-  # Sort matches by score
-  matches.sort(key=lambda x: x.distance, reverse=False)
+	# Convert images to grayscale
+	im1Gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+	im2Gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
 
-  # Remove not so good matches
-  numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
-  matches = matches[:numGoodMatches]
+	# Detect ORB features and compute descriptors.
+	orb = cv2.ORB_create(MAX_MATCHES)
+	keypoints1, descriptors1 = orb.detectAndCompute(im1Gray, None)
+	keypoints2, descriptors2 = orb.detectAndCompute(im2Gray, None)
+	
+	# Match features.
+	matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+	matches = matcher.match(descriptors1, descriptors2, None)
+	
+	# Sort matches by score
+	matches.sort(key=lambda x: x.distance, reverse=False)
 
-  # Draw top matches
-  imMatches = cv2.drawMatches(im1, keypoints1, im2, keypoints2, matches, None)
-  cv2.imwrite("matches.jpg", imMatches)
-  
-  # Extract location of good matches
-  points1 = np.zeros((len(matches), 2), dtype=np.float32)
-  points2 = np.zeros((len(matches), 2), dtype=np.float32)
+	# Remove not so good matches
+	numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
+	matches = matches[:numGoodMatches]
 
-  for i, match in enumerate(matches):
-    points1[i, :] = keypoints1[match.queryIdx].pt
-    points2[i, :] = keypoints2[match.trainIdx].pt
-  
-  # Find homography
-  h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
+	# Draw top matches
+	imMatches = cv2.drawMatches(im1, keypoints1, im2, keypoints2, matches, None)
+	cv2.imwrite("matches.jpg", imMatches)
+	
+	# Extract location of good matches
+	points1 = np.zeros((len(matches), 2), dtype=np.float32)
+	points2 = np.zeros((len(matches), 2), dtype=np.float32)
 
-  # Use homography
-  height, width, channels = im2.shape
-  im1Reg = cv2.warpPerspective(im1, h, (width, height))
-  
-  return im1Reg, h
+	for i, match in enumerate(matches):
+		points1[i, :] = keypoints1[match.queryIdx].pt
+		points2[i, :] = keypoints2[match.trainIdx].pt
+	
+	# Find homography
+	h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
+
+	# Use homography
+	height, width, channels = im2.shape
+	im1Reg = cv2.warpPerspective(im1, h, (width, height))
+	
+	return im1Reg, h
 
 
 if __name__ == '__main__':
-  im = cv2.imread("scanned-form.jpg", cv2.IMREAD_COLOR)
-  imReference = cv2.imread("form.jpg", cv2.IMREAD_COLOR)
-  
-  imReg, h = alignImages(im, imReference)
-  cv2.imwrite("aligned.jpg", imReg)
+	directory = "/home/aether/Desktop/Medical Image Segmentation/bones-unet/BONE_CT/test/female_hip (293 bad)/"
+	
+	num = 295
+
+
+	while num < 400:
+		im = cv2.imread(directory + "hip-slice" + str(num) + ".png", cv2.IMREAD_COLOR)
+		imReference = cv2.imread(directory + "hip-slice" + str(num-1) + ".png", cv2.IMREAD_COLOR)
+		print num
+		imReg, h = alignImages(im, imReference)
+		cv2.imwrite(directory + "hip-slice" + str(num) + ".png", imReg)
+		num += 1
+
+'''
+im = cv2.imread(directory + "hip-slice294.png", cv2.IMREAD_COLOR)
+imReference = cv2.imread(directory + "hip-slice292.png", cv2.IMREAD_COLOR)
+imReg, h = alignImages(im, imReference)
+cv2.imwrite(directory + "hip-slice294.png", imReg)
+'''
